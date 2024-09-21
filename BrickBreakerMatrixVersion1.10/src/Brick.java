6192570c231fd2471a6c07051ec40960306207ce
GoldenOwl24 <hlonitheletsane24@gmail.com>
2024-09-21 13:55:54 +0200

    Update Brick.java
    I instead copied my code for the brick class into this existing one. I will also be updating the gameplay class as well

--- BrickBreakerMatrixVersion1.10/src/Brick.java
+++ BrickBreakerMatrixVersion1.10/src/Brick.java
@@ -1,3 +1,68 @@
-public class Brick {
+import java.awt.Point;
+import java.awt.Color;
+import java.awt.Rectangle;
+
+public class Bricks {
+
+    private Point Position;
+    private Color colour;
+    private int strength;
+
+
+    public Bricks(Point pos, Color colour, int strength){
+        Position = pos;
+        this.colour = colour;
+        this.strength = strength;
+    }
+
+
+    public Point getPosition(){
+        return Position;
+    }
+
+    public void setPosition(Point position){
+        this.Position = position;
+       
+    }
+    public Color getColour(){
+        return colour;
+    }
+
+    public void setColour(Color colour){
+        this.colour = colour;
+    }
+
+    public int getStrength(){
+        return strength;
+    }
+
+    public void setStrength(int strength){
+        this.strength = strength;
+    }
+
+
+
+
+    public void hit(){
+        if( strength > 0){
+            strength--;
+        }
+
+    }
+
+    public boolean isDestroyed(){
+        return strength <=0;
+
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle(Postion.x, Postion.y, 50, 30);
+    }
+
+
+
+
+    
+}
     
 }
